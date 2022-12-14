886fb763411f4f971df60195fb9da76132363e9a
Avikumar Talaviya <avikumar.talaviya@gmail.com>
2022-12-14 22:06:50 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+from flask import Flask, render_template, Response
+from flask_restful import reqparse, Api
+import flask
+
+import numpy as np
 import pandas as pd
+import joblib
+import ast
+
+import os
+import json
+
+curr_path = os.path.dirname(os.path.realpath(__file__))
+
+feature_cols = ['AverageRainingDays', 'clonesize', 'AverageOfLowerTRange',
+    'AverageOfUpperTRange', 'honeybee', 'osmia', 'bumbles', 'andrena']
