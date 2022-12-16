6dc8945a351eb7cfd7a5ad2b5d9680edf26fc2a2
Avikumar Talaviya <avikumar.talaviya@gmail.com>
2022-12-16 22:27:11 +0530

    updated app.py files

--- app.py
+++ app.py
@@ -10,17 +10,61 @@ import ast
 import os
 import json
 
+from model import predict_yield
+
 curr_path = os.path.dirname(os.path.realpath(__file__))
 
 feature_cols = ['AverageRainingDays', 'clonesize', 'AverageOfLowerTRange',
     'AverageOfUpperTRange', 'honeybee', 'osmia', 'bumbles', 'andrena']
 
+context_dict = {
+    'feats': feature_cols,
+    'zip': zip,
+    'range': range,
+    'len': len,
+    'list': list,
+}
+
 app = Flask(__name__)
+api = Api(app)
+
+# # FOR FORM PARSING
+parser = reqparse.RequestParser()
+parser.add_argument('list', type=list)
 
 @app.route('/api/predict', methods=['GET','POST'])
 def api_predict():
-    return {'message':"gotcha"}
+    data = flask.request.form.get('single input')
+    
+    # converts json to int 
+    i = ast.literal_eval(data)
+    
+    y_pred = predict_yield(np.array(i).reshape(1,-1))
+    
+    
+    return {'message':"success", "pred":json.dumps(int(y_pred))}
 
+@app.route('/')
+def index():
+    
+    # render the index.html templete
+    
+    return render_template("index.html", **context_dict)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # flask.request.form.keys() will print all the input from form
+    test_data = []
+    for val in flask.request.form.values():
+        test_data.append(float(val))
+    test_data = np.array(test_data).reshape(1,-1)
+
+    y_pred = predict_yield(test_data)
+    context_dict['pred']= y_pred
+
+    print(y_pred)
+
+    return render_template('index.html', **context_dict)
 
 if __name__ == "__main__":
     app.run()
--- model.py
+++ model.py
@@ -4,3 +4,13 @@ import numpy as np
 import os
 
 curr_path = os.path.dirname(os.path.realpath(__file__))
+xgb_model = joblib.load(curr_path + "/model/wbb_xgb_model2.joblib")
+
+def predict_yield(attributes: np.ndarray):
+    """ Returns Blueberry Yield value"""
+    # print(attributes.shape) # (1,10)
+
+    pred = xgb_model.predict(attributes)
+    print("Yield predicted")
+
+    return pred[0]
