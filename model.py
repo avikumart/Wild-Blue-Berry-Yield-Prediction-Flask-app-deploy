3deeea915c1792dc95d24b9d091f29cc84727d2e
Avikumar Talaviya <avikumar.talaviya@gmail.com>
2022-12-18 18:19:55 +0530

    xgb update

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ xgb_model = joblib.load(curr_path + "/model/wbb_xgb_model2.joblib")
 
 def predict_yield(attributes: np.ndarray):
     """ Returns Blueberry Yield value"""
-    # print(attributes.shape) # (1,10)
+    # print(attributes.shape) # (1,8)
 
     pred = xgb_model.predict(attributes)
     print("Yield predicted")
